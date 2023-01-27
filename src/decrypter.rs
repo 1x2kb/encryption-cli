95fa80334e386b1ed7028b8c3fe497e158676913
1x2kb <1x2kb@protonmail.com>
2023-01-27 09:30:18 -0600

    Added decrypts data test

--- src/decrypter.rs
+++ src/decrypter.rs
@@ -13,20 +13,13 @@ pub mod rsa {
         private_key.decrypt(PaddingScheme::new_pkcs1v15_encrypt(), &data)
     }
 
-    pub fn decrypt_data_with_key_file(
-        data: Vec<u8>,
-        private_key_path: impl AsRef<Path>,
-    ) -> Result<Vec<u8>, rsa::errors::Error> {
-        decrypt_data(data, get_private_key(private_key_path))
-    }
-
     pub fn decrypt_data_file(
         file_path: impl AsRef<Path>,
         private_key_path: impl AsRef<Path>,
     ) -> Result<Vec<u8>, rsa::errors::Error> {
-        decrypt_data_with_key_file(
+        decrypt_data(
             read_file_bytes(file_path).expect("Failed to read the data file"),
-            private_key_path,
+            get_private_key(private_key_path),
         )
     }
 }
@@ -36,6 +29,8 @@ mod rsa_test {
 
     #[cfg(test)]
     mod decrypt {
+        use crate::{decrypter::rsa::decrypt_data, key::rsa::get_private_key};
+
         use super::super::rsa::decrypt_data_file;
 
         #[test]
@@ -50,6 +45,22 @@ mod rsa_test {
                 String::from_utf8(decrypted_data).unwrap(),
                 expected_decrypted_data
             );
+        }
+
+        #[test]
+        fn decrypts_data() {
+            let key_path = "test-files/keys/decryption-key.pem";
+            let encrypted_data_path = "test-files/encrypted-files/test-read-file.encrypted";
+            let expected_decrypted_data = String::from("qwerty");
+
+            let encrypted_data = std::fs::read(encrypted_data_path).unwrap();
+            let key = get_private_key(key_path);
+
+            let decrypted_data = decrypt_data(encrypted_data, key).unwrap();
+
+            let decrypted_data = String::from_utf8(decrypted_data).unwrap();
+
+            assert_eq!(decrypted_data, expected_decrypted_data);
         }
     }
 }
