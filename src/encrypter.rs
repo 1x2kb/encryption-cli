84c152a4fb5f5c7a7a922d851c6f273f44b5c3d8
1x2kb <1x2kb@protonmail.com>
2023-01-28 08:54:55 -0600

    Format

--- src/encrypter.rs
+++ src/encrypter.rs
@@ -1,19 +1,24 @@
 pub mod rsa {
-    use std::path::{Path};
+    use std::path::Path;
 
     use rsa::{PaddingScheme, PublicKey, RsaPublicKey};
 
     use crate::file::read_file_bytes;
     use crate::key::rsa::get_public_key;
 
-    pub fn encrypt_data(data: Vec<u8>, public_key: RsaPublicKey) -> Result<Vec<u8>, rsa::errors::Error> {
+    pub fn encrypt_data(
+        data: Vec<u8>,
+        public_key: RsaPublicKey,
+    ) -> Result<Vec<u8>, rsa::errors::Error> {
         let mut rng = rand::thread_rng();
 
-        public_key
-            .encrypt(&mut rng, PaddingScheme::new_pkcs1v15_encrypt(), &data)
+        public_key.encrypt(&mut rng, PaddingScheme::new_pkcs1v15_encrypt(), &data)
     }
 
-    pub fn encrypt_data_file(file_path: impl AsRef<Path>, public_key_path: impl AsRef<Path>) -> Result<Vec<u8>, rsa::errors::Error> {
+    pub fn encrypt_data_file(
+        file_path: impl AsRef<Path>,
+        public_key_path: impl AsRef<Path>,
+    ) -> Result<Vec<u8>, rsa::errors::Error> {
         let data = read_file_bytes(file_path).expect("Failed to read data file");
 
         encrypt_data(data, get_public_key(public_key_path))
@@ -25,7 +30,10 @@ mod rsa_test {
 
     #[cfg(test)]
     mod encrypts_data {
-        use crate::{encrypter::rsa::{encrypt_data, encrypt_data_file}, key::rsa::get_public_key};
+        use crate::{
+            encrypter::rsa::{encrypt_data, encrypt_data_file},
+            key::rsa::get_public_key,
+        };
 
         #[test]
         fn encrypts_raw_data() {
@@ -33,10 +41,12 @@ mod rsa_test {
             let key_path = "test-files/keys/encryption-key.pem";
 
             let unencrypted_data = std::fs::read(data_path).unwrap();
-            
-            let encrypted_data = encrypt_data(unencrypted_data.clone(), get_public_key(key_path)).unwrap();
-            assert_ne!(unencrypted_data.iter().count(), encrypted_data.iter().count());
-            let encrypted_file_bytes = std::fs::read("test-files/encrypted-files/test-read-file.encrypted").unwrap();
+
+            let encrypted_data =
+                encrypt_data(unencrypted_data.clone(), get_public_key(key_path)).unwrap();
+            assert_ne!(unencrypted_data.len(), encrypted_data.len());
+            let encrypted_file_bytes =
+                std::fs::read("test-files/encrypted-files/test-read-file.encrypted").unwrap();
             assert_ne!(encrypted_data, encrypted_file_bytes);
         }
 
@@ -48,8 +58,9 @@ mod rsa_test {
             let encrypted_data = encrypt_data_file(data_path, key_path).unwrap();
 
             let unencrypted_data = std::fs::read(data_path).unwrap();
-            assert_ne!(unencrypted_data.iter().count(), encrypted_data.iter().count());
-            let encrypted_file_bytes = std::fs::read("test-files/encrypted-files/test-read-file.encrypted").unwrap();
+            assert_ne!(unencrypted_data.len(), encrypted_data.len());
+            let encrypted_file_bytes =
+                std::fs::read("test-files/encrypted-files/test-read-file.encrypted").unwrap();
             assert_ne!(encrypted_data, encrypted_file_bytes);
         }
     }
