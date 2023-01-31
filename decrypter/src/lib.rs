a5eec6265b903c9f6ef2d779073d2bc59e32e9dc
1x2kb <1x2kb@protonmail.com>
2023-01-30 18:47:31 -0600

    Made project into workspace

--- decrypter/src/lib.rs
+++ decrypter/src/lib.rs
@@ -1,10 +1,10 @@
 pub mod rsa {
     use std::path::Path;
 
-    use crate::key::rsa::get_private_key;
+    use keys::rsa::get_private_key;
     use rsa::{PaddingScheme, RsaPrivateKey};
 
-    use crate::file::read_file_bytes;
+    use file::read_file_bytes;
 
     pub fn decrypt_data(
         data: Vec<u8>,
@@ -29,14 +29,16 @@ mod rsa_test {
 
     #[cfg(test)]
     mod decrypt {
-        use crate::{decrypter::rsa::decrypt_data, key::rsa::get_private_key};
+        use keys::rsa::get_private_key;
+
+        use super::super::rsa::decrypt_data;
 
         use super::super::rsa::decrypt_data_file;
 
         #[test]
         fn decrypts_data_file() {
-            let key_path = "test-files/keys/decryption-key.pem";
-            let data_path = "test-files/encrypted-files/test-read-file.encrypted";
+            let key_path = "../test-files/keys/decryption-key.pem";
+            let data_path = "../test-files/encrypted-files/test-read-file.encrypted";
             let expected_decrypted_data = String::from("qwerty");
 
             let decrypted_data = decrypt_data_file(data_path, key_path).unwrap();
@@ -49,8 +51,8 @@ mod rsa_test {
 
         #[test]
         fn decrypts_data() {
-            let key_path = "test-files/keys/decryption-key.pem";
-            let encrypted_data_path = "test-files/encrypted-files/test-read-file.encrypted";
+            let key_path = "../test-files/keys/decryption-key.pem";
+            let encrypted_data_path = "../test-files/encrypted-files/test-read-file.encrypted";
             let expected_decrypted_data = String::from("qwerty");
 
             let encrypted_data = std::fs::read(encrypted_data_path).unwrap();
